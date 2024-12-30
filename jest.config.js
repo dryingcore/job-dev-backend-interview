07b384ce2c230f6f9a64cd50c2ef6f43b02c6bc6
Gabriel Antunes Rocha <protonmail.default.sense07@protonmail.com>
2024-12-30 08:34:05 -0300

    jest agora ignora a pasta dist

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'], // para resolver caminhos relativos
   transform: {
